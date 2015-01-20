09bf8eab81597cbfcf829896dc98838236c5f644
Thooams <thooams@gmail.com>
2015-01-20 23:21:12 +0100

    fix tests

--- lib/ui_bibz.rb
+++ lib/ui_bibz.rb
@@ -1,30 +1,35 @@
-require 'action_view/helpers/tag_helper'
-require 'action_view/helpers/text_helper'
-require 'action_view/helpers/capture_helper'
-require 'action_view/helpers/url_helper'
 require 'action_view'
-require 'haml/helpers'
+#require 'haml/helpers'
 require 'will_paginate'
 
 module UiBibz
+
+  mattr_accessor :app_root
+
+  # Yield self on setup for nice config blocks
+  # Maybe remove ?
+  def self.setup
+    yield self
+  end
+
   module Ui
-    autoload :Component, "ui-bibz/ui/component"
-    autoload :Ui, "ui-bibz/ui/ui"
-    autoload :Nav, "ui-bibz/ui/nav"
-    autoload :Panel, "ui-bibz/ui/panel"
-    autoload :Tab, "ui-bibz/ui/tab"
-    autoload :Store, "ui-bibz/ui/store"
-    autoload :Column, "ui-bibz/ui/column"
-    autoload :Grid, "ui-bibz/ui/grid"
-    autoload :Dropdown, "ui-bibz/ui/dropdown"
-    autoload :DropdownButton, "ui-bibz/ui/dropdown_button"
+    autoload :Component, "ui_bibz/ui/component"
+    autoload :Ui, "ui_bibz/ui/ui"
+    autoload :Nav, "ui_bibz/ui/nav"
+    autoload :Panel, "ui_bibz/ui/panel"
+    autoload :Tab, "ui_bibz/ui/tab"
+    autoload :Store, "ui_bibz/ui/store"
+    autoload :Column, "ui_bibz/ui/column"
+    autoload :Grid, "ui_bibz/ui/grid"
+    autoload :Dropdown, "ui_bibz/ui/dropdown"
+    autoload :DropdownButton, "ui_bibz/ui/dropdown_button"
   end
 
   module Helpers
-    autoload :UiHelper,   "ui-bibz/helpers/ui_helper"
-    autoload :MetaHelper, "ui-bibz/helpers/meta_helper"
+    autoload :UiHelper,   "ui_bibz/helpers/ui_helper"
+    autoload :MetaHelper, "ui_bibz/helpers/meta_helper"
   end
 end
 
 
-require "ui-bibz/rails/engine"
+require "ui_bibz/rails/engine"
---
+++ test/ui_helper_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+require 'ui_helper'
+
+include UiBibz::UiHelpers
+class UiHelperTest < ActionView::TestCase
+
+  test 'panel' do
+    p = panel cls: 'toto' do |pane|
+      pane.header cls: 'header-class' do
+        "Text header"
+      end
+      pane.body 'Text body', nil, cls: 'body-toto'
+    end
+    puts p
+  end
+
+  test 'nav' do
+    n = nav({cls: 'nav-momo'}) do
+      "<a href='#'>toto</a>"
+    end
+    puts n
+  end
+
+  test 'tab' do
+    t = tab "<a href='#'>toto</a>", { active: true }, {cls: 'tab'}
+    puts t
+  end
+
+  test 'grid' do
+    g = grid cls: 'toto' do |pane|
+      pane.header 'toto'
+      pane.body cls: 'ui' do
+        'body'
+      end
+    end
+    puts g
+  end
+
+end
