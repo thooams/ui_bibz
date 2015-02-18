3babe4cc4bd214cc0f32bef812cfdd62c5a0491f
Thomas HUMMEL <thomas@hummel.link>
2015-02-18 15:52:48 +0100

    refactoring grid

--- lib/ui_bibz/ui/grid.rb
+++ lib/ui_bibz/ui/grid.rb
@@ -11,11 +11,6 @@ module UiBibz::Ui
       @options      = options || {}
       @html_options = (html_options || {}).merge({ class: 'grid' })
       @columns      = Columns.new
-      initialize_store
-      initialize_actionable
-      initialize_sortable
-      initialize_pagination
-      initialize_header
     end
 
     def columns &block
@@ -29,11 +24,14 @@ module UiBibz::Ui
     end
 
     def render
+      initialize_header
+      initialize_footer
+
       content_tag(:div, { class: cls("panel panel-default") }) do |f|
-        form_tag(url_for(controller: @store.controller, action: @store.action), method: :get) do
+        form_tag(url_for(controller: store.controller, action: store.action), method: :get) do
           concat(header_html) unless @header.nil?
           concat(body_html)   unless @body.nil?
-          concat(table_html)  unless @store.nil?
+          concat(table_html)  unless store.nil?
           concat(footer_html) unless @footer.nil?
         end
       end
@@ -41,60 +39,60 @@ module UiBibz::Ui
 
   private
 
-    def initialize_sortable
-      @sortable = Sortable.new @store, @options
+    def sort
+      @sort ||= Sortable.new store, @options
     end
 
-    def initialize_actionable
-      @actionable = Actionable.new @store, @options, @actions
+    def action
+      @action ||= Actionable.new store, @options, @actions
     end
 
-    def initialize_store
-      if @options[:store].nil?
+    def pagination
+      @pagination ||= Paginable.new store, @options
+    end
+
+    def search
+      @search ||= Searchable.new store, @options
+    end
+
+    def store
+      @store ||= if @options[:store].nil?
         raise 'Store is nil!'
       elsif @options[:store][:records].nil?
         raise 'Store can be created only with "grid_search_pagination" method!'
       else
-        @store = Store.new @options.delete :store
+        Store.new @options.delete :store
       end
     end
 
     def initialize_header
-      search  = Searchable.new @store, @options
-      @header = Component.new do
-        search.render
-      end
+      @header = Component.new search.render
     end
 
-    def initialize_pagination
-      pagination = Paginable.new @store, @options
-      if pagination.paginable?
-        @footer = Component.new do
-          pagination.render
-        end
-      end
+    def initialize_footer
+      @footer = Component.new(pagination.render) if pagination.paginable?
     end
 
     def cols
-      @columns.list.empty? ? @store.columns.list : @columns.list
+      @columns.list.empty? ? store.columns.list : @columns.list
     end
 
     def table_html
       content_tag :table, class: 'table table-hover' do
 
         ths = cols.collect do |col|
-          content_tag(:th, @sortable.header(col)) unless col.hidden?
+          content_tag(:th, sort.header(col)) unless col.hidden?
         end
 
-        ths = @actionable.header ths
+        ths = action.header ths
         concat content_tag(:tr, ths.join.html_safe)
 
-        @store.records.each do |record|
+        store.records.each do |record|
           tds = cols.collect do |col|
             content_tag(:td, td_content(record, col)) unless col.hidden?
           end
 
-          tds = @actionable.body record, tds
+          tds = action.body record, tds
           concat content_tag :tr, tds.join.html_safe
         end
       end
@@ -102,8 +100,8 @@ module UiBibz::Ui
 
     def td_content record, col
       content = record.send(col.data_index)
-      content = content.strftime(col.date_format)                         unless col.date_format.nil?
-      content = link_to content, @actionable.inject_url(col.link, record) unless col.link.nil?
+      content = content.strftime(col.date_format)                    unless col.date_format.nil?
+      content = link_to content, action.inject_url(col.link, record) unless col.link.nil?
       content
     end
 
