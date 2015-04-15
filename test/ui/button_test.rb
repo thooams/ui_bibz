1f68b93212e07d16c64325d65b08e9693c8a5cd3
Thooams <thooams@gmail.com>
2015-04-15 17:19:57 +0200

    fix button dropdown

--- test/ui/button_test.rb
+++ test/ui/button_test.rb
@@ -11,10 +11,10 @@ class ButtonTest < ActionView::TestCase
   end
 
   test 'button group' do
-    actual = UiBibz::Ui::ButtonGroup.new() do
+    actual = UiBibz::Ui::ButtonGroup.new position: :vertical do
       UiBibz::Ui::Button.new('toto').render
     end.render
-    expected = "<div class=\"btn-group\"><button class=\"btn-default btn\">toto</button></div>"
+    expected = "<div class=\"btn-group btn-group-vertical\" role=\"role\"><button class=\"btn-default btn\">toto</button></div>"
 
     assert_equal expected, actual
   end
@@ -22,6 +22,19 @@ class ButtonTest < ActionView::TestCase
   test 'link button' do
     actual   = UiBibz::Ui::ButtonLink.new('Toto', { url: users_path, state: :danger, glyph: 'add'}).render
     expected = "<a class=\"btn-danger btn\" href=\"/users\"><i class=\"glyph fa fa-add\"></i> Toto</a>"
+
+    assert_equal expected, actual
+  end
+
+  test 'button dropdown' do
+      actual = UiBibz::Ui::ButtonDropdown.new("Dropdown", type: :dropup, state: :success).tap do |d|
+      d.list 'toto'
+      d.list 'header', type: :header
+      d.list 'momo'
+      d.list '---'
+      d.list 'lolo'
+    end.render
+    expected = "<div class=\"btn-group dropup\" role=\"group\"><button class=\"btn btn-success dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></button><ul class=\"dropdown-menu dropdown-menu-left\" role=\"menu\"><li role=\"presentation\">toto</li><li class=\"dropdown-header\" role=\"presentation\">header</li><li role=\"presentation\">momo</li><li class=\"divider\" role=\"presentation\"></li><li role=\"presentation\">lolo</li></ul></div>"
 
     assert_equal expected, actual
   end
