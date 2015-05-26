78003ec8f2d8bc21de4b5614409062b7336d2e73
Thooams <thooams@gmail.com>
2015-05-26 17:30:20 +0200

    add navbar

--- lib/ui_bibz/helpers/ui_core_helper.rb
+++ lib/ui_bibz/helpers/ui_core_helper.rb
@@ -26,6 +26,10 @@ module UiBibz::Helpers::UiCoreHelper
     UiBibz::Ui::Core::Nav.new(content, options, html_options).tap(&block).render
   end
 
+  def navbar content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Navbar.new(content, options, html_options).tap(&block).render
+  end
+
   def list_group content = nil, options = nil, html_options = nil, &block
     UiBibz::Ui::Core::ListGroup.new(content, options, html_options).tap(&block).render
   end
