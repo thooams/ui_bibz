271aec8dfcf191617618a82f9766d0a253e40071
Thomas HUMMEL <thomas@hummel.link>
2015-09-02 11:12:35 +0200

    fix dropdown divider

--- test/ui/dropdown_test.rb
+++ test/ui/dropdown_test.rb
@@ -10,7 +10,7 @@ class DropdownTest < ActionView::TestCase
       d.divider
       d.link 'Link3', url: '#link3'
     end.render
-    expected = "<div class=\"dropup\"><button class=\"btn btn-success dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></button><ul class=\"dropdown-menu dropdown-menu-left\" role=\"menu\"><li class=\"\" role=\"presentation\"><a href=\"#link1\"><i class=\"glyph fa fa-eye\"></i> Link 1</a></li><li class=\"dropdown-header\" role=\"presentation\">header</li><li class=\"\" role=\"presentation\"><a href=\"#link2\">Link 2</a></li><li class=\"dropdown-divider\"></li><li class=\"\" role=\"presentation\"><a href=\"#link3\">Link3</a></li></ul></div>"
+    expected = "<div class=\"dropup\"><button class=\"btn btn-success dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></button><ul class=\"dropdown-menu dropdown-menu-left\" role=\"menu\"><li class=\"\" role=\"presentation\"><a href=\"#link1\"><i class=\"glyph fa fa-eye\"></i> Link 1</a></li><li class=\"dropdown-header\" role=\"presentation\">header</li><li class=\"\" role=\"presentation\"><a href=\"#link2\">Link 2</a></li><li class=\"divider\" role=\"separator\"></li><li class=\"\" role=\"presentation\"><a href=\"#link3\">Link3</a></li></ul></div>"
 
     assert_equal expected, actual
   end
