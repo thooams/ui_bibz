fe9aa5f006c0a433c966191c33bc2a8077e62fb5
Thooams <thooams@gmail.com>
2015-09-14 00:02:47 +0200

    add card_group

--- lib/ui_bibz/helpers/ui_core_helper.rb
+++ lib/ui_bibz/helpers/ui_core_helper.rb
@@ -16,6 +16,10 @@ module UiBibz::Helpers::UiCoreHelper
     end
   end
 
+  def card_group content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::CardGroup.new(content, options, html_options).tap(&block).render
+  end
+
   # Use "notify" instead of "alert" : due to "actionview (4.2.0) lib/action_view/helpers/form_helper.rb" conflict
   def notify content = nil, options = nil, html_options = nil, &block
     UiBibz::Ui::Core::Alert.new(content, options, html_options, &block).render
