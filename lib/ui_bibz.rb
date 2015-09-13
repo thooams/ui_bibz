5e57e000d122b1a2143c9272ba297897febef79d
Thomas HUMMEL <thomas@hummel.link>
2015-09-14 00:02:47 +0200

    add card_group

--- lib/ui_bibz.rb
+++ lib/ui_bibz.rb
@@ -21,6 +21,7 @@ module UiBibz
       autoload :ButtonLink,              "ui_bibz/ui/core/button/button_link"
       autoload :ButtonSplitDropdown,     "ui_bibz/ui/core/button/button_split_dropdown"
       autoload :Card,                    "ui_bibz/ui/core/card/card"
+      autoload :CardGroup,               "ui_bibz/ui/core/card/card_group"
       autoload :Col,                     "ui_bibz/ui/core/col"
       autoload :Component,               "ui_bibz/ui/core/component"
       autoload :Container,               "ui_bibz/ui/core/container"
---
+++ lib/ui_bibz/ui/core/card/card_group.rb
@@ -0,0 +1,48 @@
+module UiBibz::Ui::Core
+
+  # Create a card group
+  #
+  # ==== Attributes
+  #
+  # * +content+ - Content of element
+  # * +options+ - Options of element
+  # * +html_options+ - Html Options of element
+  #
+  # ==== Options
+  #
+  # You can add HTML attributes using the +html_options+.
+  # You can pass arguments in options attribute:
+  #
+  # ==== Signatures
+  #
+  #   UiBibz::Ui::Core::CardGroup.new(content, options = nil, html_options = nil)
+  #
+  #   UiBibz::Ui::Core::CardGroup.new(options = nil, html_options = nil) do
+  #     content
+  #   end
+  #
+  # ==== Examples
+  #
+  #   UiBibz::Ui::Core::CardGroup.new do |cg|
+  #     cg.card 'Exemple'
+  #   end.render
+  #
+  class CardGroup < Component
+
+    # See UiBibz::Ui::Core::Component.initialize
+    def initialize content = nil, options = nil, html_options = nil, &block
+      super
+      @items = []
+    end
+
+    # Render html tag
+    def render
+      content_tag :div, @items.join.html_safe, class_and_html_options("card-group")
+    end
+
+    def card content = nil, options = nil, html_options = nil, &block
+      @items << UiBibz::Ui::Core::Card.new(content, options, html_options, &block).render
+    end
+
+  end
+end
