0c38cb8eaa906106a7ba377706f97422f9e4f6dc
Thooams <thooams@gmail.com>
2016-02-03 14:54:45 +0100

    add surround_input

---
+++ app/inputs/custom_inputs/surround_input.rb
@@ -0,0 +1,10 @@
+module CustomInputs
+  class SurroundInput < SimpleForm::Inputs::StringInput
+    include UiBibz::Ui::Core
+
+    def input(wrapper_options)
+      UiBibz::Ui::Core::SurroundField.new(attribute_name, input_options, input_html_options).render
+    end
+
+  end
+end
