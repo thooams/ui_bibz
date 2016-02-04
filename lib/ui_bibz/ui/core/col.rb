ce8df2bb887504537a72370b29de9ee701e80bcd
Thomas HUMMEL <thomas@hummel.link>
2016-02-04 15:48:24 +0100

    update col component

--- lib/ui_bibz/ui/core/col.rb
+++ lib/ui_bibz/ui/core/col.rb
@@ -3,6 +3,7 @@ module UiBibz::Ui::Core
   # Create a col
   #
   # This element is an extend of UiBibz::Ui::Core::Component.
+  # size are xs, sm, md, lg, xl
   #
   # ==== Attributes
   #
@@ -16,7 +17,13 @@ module UiBibz::Ui::Core
   # You can pass arguments in options attribute:
   # * +num+ - Integer | 0..12
   # * +offset+ - Integer
-  # * +size+ - Integer
+  # * +pull+ - Integer
+  # * +push+ - Integer
+  # * +xs+ - Hash
+  # * +sm+ - Hash
+  # * +md+ - Hash
+  # * +lg+ - Hash
+  # * +xl+ - Hash
   #
   # ==== Signatures
   #
@@ -28,11 +35,9 @@ module UiBibz::Ui::Core
   #
   # ==== Examples
   #
-  #   UiBibz::Ui::Core::Col.new({num: 2, offset: 1, size: 3}, class: 'test') do
-  #     #content
-  #   end
+  #   UiBibz::Ui::Core::Col.new('content', { num: 2, offset: 1 }, { class: 'test' })
   #
-  #   UiBibz::Ui::Core::Col.new([{num: 2, offset: 1, size: 3}, { num: 3}], class: 'test') do
+  #   UiBibz::Ui::Core::Col.new(xs: { num: 2, pull: 1, push: 1}, md: { num: 3 }, { class: 'test' }) do
   #     #content
   #   end
   #
@@ -47,7 +52,6 @@ module UiBibz::Ui::Core
     # See UiBibz::Ui::Core::Component.initialize
     def initialize content = nil, options = nil, html_options = nil, &block
       super
-      @cols = []
     end
 
     # Render html tag
@@ -58,24 +62,36 @@ module UiBibz::Ui::Core
   private
 
     def col_classes
-      classes = []
-      [@options].flatten.each do |col|
-        classes << add_classes("col-#{ size(col) }-#{ num(col) }", offset(col))
+      kl = []
+      @options.each do |ke,va|
+        kl << write_classes(ke, va) if %i(xs sm md lg xl).include?(ke)
       end
-      @options = {}
-      classes.join(' ')
+      kl << write_classes(:md, @options) if kl.empty?
+      kl.join(' ')
     end
 
-    def size col = {}
-      col[:size] || :md
+    def write_classes size, opts
+      opts.map{ |k, v| send(k, size.to_sym, v) if %i(num offset push pull).include?(k) }.join(' ')
     end
 
-    def num col
-      col[:num] || 1
+    # col-md-9
+    def num size, n
+      "col-#{ size }-#{ n }"
     end
 
-    def offset col
-      "col-#{ size }-offset-#{ col[:offset] }" if col[:offset]
+    # col-md-offset-9
+    def offset size, n
+      "col-#{ size }-offset-#{ n }"
+    end
+
+    # col-md-push-9
+    def push size, n
+      "col-#{ size }-push-#{ n }"
+    end
+
+    # col-md-pull-9
+    def pull size, n
+      "col-#{ size }-pull-#{ n }"
     end
 
   end
