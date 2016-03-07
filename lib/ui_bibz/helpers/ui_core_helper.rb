a605b91749ccaeccd0e2020b224f16e7280f5b44
Jeremy <jgontard@codde.fr>
2016-03-07 14:38:08 +0100

    Add switch_field in doc, add Nav menu for components navs and navbars

--- lib/ui_bibz/helpers/ui_core_helper.rb
+++ lib/ui_bibz/helpers/ui_core_helper.rb
@@ -1,5 +1,32 @@
 module UiBibz::Helpers::UiCoreHelper
 
+  # Button section begin ----------------------------------------------------
+  def button content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Button.new(content, options, html_options, &block).render
+  end
+
+  def button_choice content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::ButtonChoice.new(content, options, html_options, &block).render
+  end
+
+  def button_dropdown name, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::ButtonDropdown.new(name, options, html_options).tap(&block).render
+  end
+
+  def button_group content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::ButtonGroup.new(content, options, html_options, &block).render
+  end
+
+  def button_link content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::ButtonLink.new(content, options, html_options, &block).render
+  end
+
+  def button_split_dropdown name, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::ButtonSplitDropdown.new(name, options, html_options).tap(&block).render
+  end
+  # Button section end ----------------------------------------------------
+
+  # Card section begin ----------------------------------------------------
   def card content = nil, options = nil, html_options = nil, &block
     if is_tap(content, options)
       UiBibz::Ui::Core::Card.new(content, options, html_options).tap(&block).render
@@ -19,39 +46,11 @@ module UiBibz::Helpers::UiCoreHelper
   def card_column content = nil, options = nil, html_options = nil, &block
     UiBibz::Ui::Core::CardColumn.new(content, options, html_options).tap(&block).render
   end
+  # Card section end ----------------------------------------------------
 
+  # Input section begin ----------------------------------------------------
   def date_picker_field content = nil, options = nil, html_options = nil, &block
     UiBibz::Ui::Core::DatePickerField.new(content, options, html_options, &block).render
-  end
-
-  def modal content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Modal.new(content, options, html_options).tap(&block).render
-  end
-
-  # Use "notify" instead of "alert" : due to "actionview (4.2.0) lib/action_view/helpers/form_helper.rb" conflict
-  def notify content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Alert.new(content, options, html_options, &block).render
-  end
-
-  def stars content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Stars.new(content, options, html_options, &block).render
-  end
-
-  # Use "etiquette" instead of "label" : due to "actionview (4.2.0) lib/action_view/helpers/form_helper.rb" conflict
-  def etiquette content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Label.new(content, options, html_options, &block).render
-  end
-
-  def jumbotron content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Jumbotron.new(content, options, html_options, &block).render
-  end
-
-  def nav content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Nav.new(content, options, html_options).tap(&block).render
-  end
-
-  def surround_field content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::SurroundField.new(content, options, html_options, &block).render
   end
 
   def multi_column_field content = nil, options = nil, html_options = nil, &block
@@ -62,39 +61,44 @@ module UiBibz::Helpers::UiCoreHelper
     UiBibz::Ui::Core::MultiSelectField.new(content, options, html_options, &block).render
   end
 
+  def surround_field content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::SurroundField.new(content, options, html_options, &block).render
+  end
+
+  def switch_field content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::SwitchField.new(content, options, html_options, &block).render
+  end
+  # Input section end ----------------------------------------------------
+
+  # Nav section begin ----------------------------------------------------
+  def nav content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Nav.new(content, options, html_options).tap(&block).render
+  end
+
   def navbar content = nil, options = nil, html_options = nil, &block
     UiBibz::Ui::Core::Navbar.new(content, options, html_options).tap(&block).render
   end
+  # Nav section end ----------------------------------------------------
 
-  def list_group content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::ListGroup.new(content, options, html_options).tap(&block).render
+  # Layout section begin ----------------------------------------------------
+  def row content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Row.new(content, options, html_options, &block).render
   end
 
-  # Button section begin ----------------------------------------------------
-  def button_link content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::ButtonLink.new(content, options, html_options, &block).render
+  def col content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Col.new(content, options, html_options, &block).render
   end
 
-  def button_choice content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::ButtonChoice.new(content, options, html_options, &block).render
+  def container content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Container.new(content, options, html_options, &block).render
   end
+  # Layout section end ----------------------------------------------------
 
-  def button_group content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::ButtonGroup.new(content, options, html_options, &block).render
+  # Other section begin ----------------------------------------------------
+  # Use "notify" instead of "alert" : due to "actionview (4.2.0) lib/action_view/helpers/form_helper.rb" conflict
+  def notify content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Alert.new(content, options, html_options, &block).render
   end
-
-  def button content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Button.new(content, options, html_options, &block).render
-  end
-
-  def button_dropdown name, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::ButtonDropdown.new(name, options, html_options).tap(&block).render
-  end
-
-  def button_split_dropdown name, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::ButtonSplitDropdown.new(name, options, html_options).tap(&block).render
-  end
-  # Button section end ----------------------------------------------------
 
   def breadcrumb content = nil, options = nil, html_options = nil, &block
     if is_tap(content, options)
@@ -112,21 +116,31 @@ module UiBibz::Helpers::UiCoreHelper
     UiBibz::Ui::Core::Glyph.new(content, options, html_options, &block).render
   end
 
+  def jumbotron content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Jumbotron.new(content, options, html_options, &block).render
+  end
+
+  # Use "etiquette" instead of "label" : due to "actionview (4.2.0) lib/action_view/helpers/form_helper.rb" conflict
+  def etiquette content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Label.new(content, options, html_options, &block).render
+  end
+
+  def list_group content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::ListGroup.new(content, options, html_options).tap(&block).render
+  end
+
+  def modal content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Modal.new(content, options, html_options).tap(&block).render
+  end
+
   def progress percentage = nil, options = nil, html_options = nil, &block
     UiBibz::Ui::Core::Progress.new(percentage, options, html_options, &block).render
   end
 
-  def row content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Row.new(content, options, html_options, &block).render
+  def stars content = nil, options = nil, html_options = nil, &block
+    UiBibz::Ui::Core::Stars.new(content, options, html_options, &block).render
   end
-
-  def col content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Col.new(content, options, html_options, &block).render
-  end
-
-  def container content = nil, options = nil, html_options = nil, &block
-    UiBibz::Ui::Core::Container.new(content, options, html_options, &block).render
-  end
+  # Other section end ----------------------------------------------------
 
 private
 
