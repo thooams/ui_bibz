82388878fde3dda482f3a575b1529ef044a0b4e0
Jeremy <jgontard@codde.fr>
2016-03-16 15:45:35 +0100

    Fix display for bootstrap dropdown, Fix turbolink issues with JS

--- app/assets/javascripts/fix-bootstrap.js
+++ app/assets/javascripts/fix-bootstrap.js
@@ -1,3 +1,8 @@
+// Fix turbolink issues#21
+// https://github.com/thooams/Ui-Bibz/issues/21
+
 $(document).on('page:change', function() {
-  $('.dropdown-toggle').dropdown();
+  $('.dropdown-toggle').dropdown();         // Fix dropdown toggle
+  $('.switch').bootstrapSwitch();           // Fix switch field
+  $('.multi-select').multiSelect();         // Fix multiselect
 });
