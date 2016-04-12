bd64cef1d291b5bb08ff985ad20aebaed36b4406
Thomas HUMMEL <thomas@hummel.link>
2016-04-12 14:39:24 +0200

    update switch options

--- app/inputs/custom_inputs/autocomplete_input.rb
+++ app/inputs/custom_inputs/autocomplete_input.rb
@@ -5,11 +5,7 @@ module CustomInputs
 
     def input(wrapper_options)
       label_method, value_method = detect_collection_methods
-      ap label_method
-      ap value_method
-      ap @options
       options = options || {}
-      ap collection
       options = options.merge({ builder: @builder })
       options = options.merge(option_tags: options_from_collection_for_select(collection, label_method, label_method))
       UiBibz::Ui::Core::AutocompleteField.new(attribute_name, options, input_html_options).render
