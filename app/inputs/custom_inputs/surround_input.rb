31596ff3befa3de2d6e4b05fef9f06fd9a9d4700
Thomas HUMMEL <thomas@hummel.link>
2016-04-18 15:28:32 +0200

    fix surround_input for simple_form

--- app/inputs/custom_inputs/surround_input.rb
+++ app/inputs/custom_inputs/surround_input.rb
@@ -3,8 +3,7 @@ module CustomInputs
     include UiBibz::Ui::Core
 
     def input(wrapper_options)
-      options = options || {}
-      options = options.merge({ builder: @builder })
+      options = @options.merge({ builder: @builder })
       UiBibz::Ui::Core::SurroundField.new(attribute_name, options, input_html_options).render
     end
 
