3c130898136efb2768c39533412041e8f3d16324
Thomas HUMMEL <thomas@hummel.link>
2016-05-17 16:51:52 +0200

    fix tests

--- test/ui/inputs_test.rb
+++ test/ui/inputs_test.rb
@@ -89,7 +89,7 @@ class InputsTest < ActionView::TestCase
 
     test 'formula_field' do
       actual = UiBibz::Ui::Core::FormulaField.new('value').render
-      expected = "<div class=\"input-group formula_field\"><input type=\"text\" name=\"value\" id=\"value\" class=\"formula_field_input form-control\" /><span class=\"formula_field_sign input-group-addon\">=</span><input type=\"text\" name=\"value_formula\" id=\"value_formula\" value=\"\" readonly=\"readonly\" class=\"formula_field_result form-control\" /><span class=\"formula_field_alert input-group-addon\" data-toggle=\"tooltip\"><i class=\"glyph-danger glyph fa fa-exclamation-triangle\"></i></span></div>"
+      expected = "<div class=\"input-group formula_field\"><input type=\"text\" name=\"value_formula\" id=\"value_formula\" value=\"\" class=\"formula_field_input form-control\" /><span class=\"formula_field_sign input-group-addon\">=</span><input type=\"text\" name=\"value\" id=\"value\" readonly=\"readonly\" class=\"formula_field_result form-control\" /><span class=\"formula_field_alert input-group-addon\" data-toggle=\"tooltip\"><i class=\"glyph-danger glyph fa fa-exclamation-triangle\"></i></span></div>"
 
       assert_equal expected, actual
     end
