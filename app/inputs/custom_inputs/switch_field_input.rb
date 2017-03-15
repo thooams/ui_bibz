417b7f5e5ea9aaf184b0513497f7e70a67f216a6
Thomas HUMMEL <thomas@hummel.link>
2017-03-15 15:12:26 +0100

    [...work in progress...]

--- app/inputs/custom_inputs/switch_field_input.rb
+++ app/inputs/custom_inputs/switch_field_input.rb
@@ -3,7 +3,7 @@ module CustomInputs
     include UiBibz::Ui::Core::Forms::Choices
 
     def input(wrapper_options)
-      UiBibz::Ui::Core::Forms::Choices::SwitchField.new(input_attribute_name, new_options, new_input_html_options).render
+      UiBibz::Ui::Core::Forms::Choices::SwitchField.new(input_attribute_name, options, new_input_html_options).render
     end
 
     def new_options
