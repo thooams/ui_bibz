42dc249ce0f3a5d12772001e50c17c41d70c8529
Thomas HUMMEL <thomas@hummel.link>
2017-10-25 16:48:34 +0200

    update modal component

--- lib/ui_bibz/ui/core/windows/components/modal_header.rb
+++ lib/ui_bibz/ui/core/windows/components/modal_header.rb
@@ -39,8 +39,8 @@ module UiBibz::Ui::Core::Windows::Components
     # Render html tag
     def render
       content_tag :div, html_options do
+        concat content_tag :h5, glyph_and_content_html, class: 'modal-title'
         concat close_button_html
-        concat content_tag :h4, glyph_and_content_html, class: 'modal-title'
       end
     end
 
