d369a9c6dc3cf7a20a0d96abc4190ff67ce31b7a
Thomas HUMMEL <thomas@hummel.link>
2017-12-01 11:21:29 +0100

    check if simple form is defined in app

--- config/initializers/ui_bibz.rb
+++ config/initializers/ui_bibz.rb
@@ -1,7 +1,9 @@
-SimpleForm.setup do |config|
-  # Namespaces where SimpleForm should look for custom input classes that
-  # override default inputs.
-  config.custom_inputs_namespaces << "UiBibzInputs"
+if defined?(SimpleForm)
+  SimpleForm.setup do |config|
+    # Namespaces where SimpleForm should look for custom input classes that
+    # override default inputs.
+    config.custom_inputs_namespaces << "UiBibzInputs"
+  end
 end
 
 Rails.application.config.autoload_paths += Dir[File.join(Rails.root, "app", "ui", "**", "*.rb")].each {|l| require l }
