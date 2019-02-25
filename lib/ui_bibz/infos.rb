8496fa10a3c6a35c0b8fd35cb6df1ebd43481657
Thomas HUMMEL <thomas@hummel.link>
2019-02-25 14:56:21 +0100

    update fontawesome vendors and update gems

--- lib/ui_bibz/infos.rb
+++ lib/ui_bibz/infos.rb
@@ -1,9 +1,9 @@
 module UiBibz
   NAME                = "Ui Bibz"
-  VERSION             = "2.3.3"
+  VERSION             = "2.3.4"
   DESCRIPTION         = "A Rails Interface Framework using Bootstrap."
   SUMMARY             = "Create your project with Ui Bibz. Over a thirty reusable components built to provide iconography, dropdowns, input groups, navigation, alerts, and much more."
   LICENSE             = "MIT"
-  FONTAWESOME_VERSION = "5.7.0"
+  FONTAWESOME_VERSION = "5.7.2"
   BOOTSTRAP_VERSION   = "4.3.1"
 end
