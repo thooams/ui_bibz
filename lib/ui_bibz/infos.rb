5ce1e242abfe9886fd06c675b5dfbdd209dcc056
Thomas HUMMEL <thomas@hummel.link>
2020-03-04 22:26:13 +1100

    Update font awesome and bootstrap gems

--- lib/ui_bibz/infos.rb
+++ lib/ui_bibz/infos.rb
@@ -1,9 +1,9 @@
 module UiBibz
   NAME                = "Ui Bibz"
-  VERSION             = "2.3.15"
+  VERSION             = "2.4.0"
   DESCRIPTION         = "A Rails Interface Framework using Bootstrap."
   SUMMARY             = "Create your project with Ui Bibz. Over a thirty reusable components built to provide iconography, dropdowns, input groups, navigation, alerts, and much more."
   LICENSE             = "MIT"
-  FONTAWESOME_VERSION = "5.10.1"
-  BOOTSTRAP_VERSION   = "4.3.1"
+  FONTAWESOME_VERSION = "5.12.1"
+  BOOTSTRAP_VERSION   = "4.4.1"
 end
