7143d5a1de997ad53728b9a40ba9e412c022c1ea
Thomas HUMMEL <thomas@hummel.link>
2020-06-09 20:25:37 +1000

    Update gems

--- lib/ui_bibz/infos.rb
+++ lib/ui_bibz/infos.rb
@@ -2,7 +2,7 @@
 
 module UiBibz
   NAME                = 'Ui Bibz'
-  VERSION             = '2.5.4'
+  VERSION             = '2.5.5'
   DESCRIPTION         = 'A Rails Interface Framework using Bootstrap.'
   SUMMARY             = 'Create your project with Ui Bibz. Over a thirty reusable components built to provide iconography, dropdowns, input groups, navigation, alerts, and much more.'
   LICENSE             = 'MIT'
