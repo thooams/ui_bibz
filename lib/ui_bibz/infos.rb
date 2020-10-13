d6a805c351055cf7c23e1d7812a10bdb6f2e83ad
Thomas HUMMEL <thomas@hummel.link>
2020-10-13 13:17:23 +0200

    Update infos

--- lib/ui_bibz/infos.rb
+++ lib/ui_bibz/infos.rb
@@ -3,15 +3,15 @@
 module UiBibz
   NAME                = 'Ui Bibz'
   SLUG                = 'ui_bibz'
-  VERSION             = '3.0.0'
-  DESCRIPTION         = 'A Rails Interface Framework using Bootstrap.'
-  SUMMARY             = 'Create your project with Ui Bibz. Over a thirty reusable components built to provide iconography, dropdowns, input groups, navigation, alerts, and much more.'
+  BOOTSTRAP_VERSION   = '5.0.0.alpha2'
+  DESCRIPTION         = "A Rails Interface Framework using Bootstrap #{BOOTSTRAP_VERSION}."
+  SUMMARY             = 'Create your project with Ui Bibz. Over a thirty reusable components built to provide dropdowns, input groups, navigation, alerts, and much more without writing html.'
   LICENSE             = 'MIT'
   FONTAWESOME_VERSION = '5.14.0'
-  BOOTSTRAP_VERSION   = '5.0.0.alpha2'
   HOMEPAGE            = 'http://thooams.github.io/Ui-Bibz/'
   REPO                = 'git+https://github.com/thooams/Ui-Bibz.git'
   EMAIL               = 'thomas@hummel.link'
   AUTHOR              = 'Thooams'
+  VERSION             = '3.0.0.alpha2'
   AUTHORS = ['Thooams'].freeze
 end
