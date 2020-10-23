410c1fc937ce29ea72a424e23c8843102f045db5
Thomas HUMMEL <thomas@hummel.link>
2020-10-23 10:57:49 +0200

    Fix checkbox tests

--- test/ui/core/forms/choices/checkbox_field_test.rb
+++ test/ui/core/forms/choices/checkbox_field_test.rb
@@ -5,6 +5,13 @@ require 'test_helper'
 class CheckboxFieldTest < ActionView::TestCase
   test 'checkbox_field' do
     actual = UiBibz::Ui::Core::Forms::Choices::CheckboxField.new('John', { value: 1 }).render
+    expected = "<div class=\"form-check\"><input type=\"checkbox\" name=\"John\" id=\"John\" value=\"1\" class=\"form-check-input\" /><label class=\"form-check-label\" for=\"John\">John</label></div>"
+
+    assert_equal expected, actual
+  end
+
+  test 'checkbox_field which is a boolean' do
+    actual = UiBibz::Ui::Core::Forms::Choices::CheckboxField.new('John', { boolean: true, value: 1 }).render
     expected = "<div class=\"form-check\"><input type=\"hidden\" name=\"John\" id=\"John-hidden\" value=\"0\" /><input type=\"checkbox\" name=\"John\" id=\"John\" value=\"1\" class=\"form-check-input\" /><label class=\"form-check-label\" for=\"John\">John</label></div>"
 
     assert_equal expected, actual
