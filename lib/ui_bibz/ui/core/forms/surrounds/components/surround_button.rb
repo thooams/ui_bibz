075109e5e12bdaead0f5e15734b1f66aa55efc54
Thomas HUMMEL <thomas@hummel.link>
2020-11-12 17:39:19 +0100

    Remove wrapper in the surround component

--- lib/ui_bibz/ui/core/forms/surrounds/components/surround_button.rb
+++ lib/ui_bibz/ui/core/forms/surrounds/components/surround_button.rb
@@ -5,9 +5,7 @@ module UiBibz::Ui::Core::Forms::Surrounds
     # See UiBibz::Ui::Core::Dropdown.initialize
 
     def pre_render
-      content_tag :div, class: 'input-group-btn' do
-        button_html_tag
-      end
+      button_html_tag
     end
   end
 end
