96eb6763f285b30ecec03b83f9d07ce34c7138de
Thomas HUMMEL <thomas@hummel.link>
2020-12-14 20:33:29 +0100

    Fix tests

--- test/ui/core/windows/modal_test.rb
+++ test/ui/core/windows/modal_test.rb
@@ -28,7 +28,7 @@ class UiHelperTest < ActionView::TestCase
         concat ui_button_link 'Save', url: '#', status: :primary
       end
     end
-    expected = "<div data-backdrop=\"static\" data-keyboard=\"false\" class=\"modal\" tabindex=\"-1\" aria-labelledby=\"My title\" aria-hidden=\"true\"><div class=\"modal-dialog modal-fullscreen modal-dialog-scrollable\"><div class=\"modal-content\"><div class=\"modal-header\"><h5 class=\"modal-title\">My title</h5><button class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button></div><div class=\"modal-body\">My body</div><div class=\"modal-footer\"><a class=\"btn-link btn\" role=\"button\" href=\"#\">Close</a><a class=\"btn-primary btn\" role=\"button\" href=\"#\">Save</a></div></div></div></div>"
+    expected = "<div data-bs-backdrop=\"static\" data-bs-keyboard=\"false\" class=\"modal\" tabindex=\"-1\" aria-labelledby=\"My title\" aria-hidden=\"true\"><div class=\"modal-dialog modal-fullscreen modal-dialog-scrollable\"><div class=\"modal-content\"><div class=\"modal-header\"><h5 class=\"modal-title\">My title</h5><button class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button></div><div class=\"modal-body\">My body</div><div class=\"modal-footer\"><a class=\"btn-link btn\" role=\"button\" href=\"#\">Close</a><a class=\"btn-primary btn\" role=\"button\" href=\"#\">Save</a></div></div></div></div>"
 
     assert_equal expected, actual
   end
