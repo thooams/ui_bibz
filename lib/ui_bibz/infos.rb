44e57b50f47475562daf2af2ca927d235409bf10
Thomas HUMMEL <thomas@hummel.link>
2021-02-05 09:46:01 +0100

    Update gems

--- lib/ui_bibz/infos.rb
+++ lib/ui_bibz/infos.rb
@@ -12,6 +12,6 @@ module UiBibz
   REPO                = 'git+https://github.com/thooams/ui_bibz.git'
   EMAIL               = 'thomas@hummel.link'
   AUTHOR              = 'Thooams [Thomas HUMMEL]'
-  VERSION             = '3.0.0.beta7'
+  VERSION             = '3.0.0.beta8'
   AUTHORS = ['Thooams'].freeze
 end
