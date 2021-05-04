d0a5a3e996ddc07d4873c13c8290e797befb6d0e
Thomas HUMMEL <thomas@hummel.link>
2021-05-04 11:24:58 +0200

    Update gems

--- lib/ui_bibz/infos.rb
+++ lib/ui_bibz/infos.rb
@@ -7,11 +7,11 @@ module UiBibz
   DESCRIPTION         = "A Rails Interface Framework using Bootstrap #{BOOTSTRAP_VERSION}."
   SUMMARY             = "Ui Bibz is an Ui Framework that allows you to build an interface very quickly and simply using Bootstrap #{BOOTSTRAP_VERSION}."
   LICENSE             = 'MIT'
-  FONTAWESOME_VERSION = '5.15.2'
+  FONTAWESOME_VERSION = '5.15.3'
   HOMEPAGE            = 'https://hummel.link/ui-bibz/'
   REPO                = 'git+https://github.com/thooams/ui_bibz.git'
   EMAIL               = 'thomas@hummel.link'
   AUTHOR              = 'Thooams [Thomas HUMMEL]'
-  VERSION             = '3.0.0.beta17'
+  VERSION             = '3.0.0.beta18'
   AUTHORS = ['Thooams'].freeze
 end
