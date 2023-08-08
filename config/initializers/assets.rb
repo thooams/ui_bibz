d6a3c072a2e183adc096e8bd54beed081ca9fb78
Thomas HUMMEL <thomas@hummel.link>
2023-08-08 16:08:05 +0200

    Add js to importmap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@
 
 Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js
 jquery-3.7.0.min.js bootstrap-multiselect.min.js jquery.multi-select.min.js
-forms.js interfaces.js tables.js"]
+forms.js interfaces.js tables.js forms/formula.js forms/input-connected.js forms/jquery.multi-select-extend.js"]
