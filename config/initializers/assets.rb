98d08fcc77ddda115faf96fbafb339ff1d9407ad
Thomas HUMMEL <thomas@hummel.link>
2023-08-08 15:14:38 +0200

    [...work in progress...]

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js
-                                                 jquery-3.7.0.min.js bootstrap-multiselect.min.js jquery.multi-select.min.js"]
+jquery-3.7.0.min.js bootstrap-multiselect.min.js jquery.multi-select.min.js
+forms.js interfaces.js tables.js"]
