06dd2d343cc06c75548899503a7f3de3773cd209
Thomas HUMMEL <thomas@hummel.link>
2023-08-10 18:00:15 +0200

    Add fuzzysort to manifest.js

--- app/assets/config/ui_bibz.js
+++ app/assets/config/ui_bibz.js
@@ -1,5 +1,6 @@
 //= link_tree ../../../vendor/assets/javascripts .js
 ////= link_directory ../javascripts .js
+//= link fuzzysort.js
 //= link jquery.multi-select.min.js
 //= link bootstrap-markdown.js
 //= link bootstrap-datepicker.min.js
