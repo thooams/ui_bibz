2eb850c492015cb5f0227f901ee912293c785134
Thomas HUMMEL <thomas@hummel.link>
2023-09-26 18:00:06 +0200

    Update version

--- lib/ui_bibz/infos.rb
+++ lib/ui_bibz/infos.rb
@@ -12,6 +12,6 @@ module UiBibz
   REPO                = 'git+https://github.com/thooams/ui_bibz.git'
   EMAIL               = 'thomas@hummel.link'
   AUTHOR              = 'Thooams [Thomas HUMMEL]'
-  VERSION             = '4.0.0.beta'
+  VERSION             = '4.0.0.beta2'
   AUTHORS = ['Thooams'].freeze
 end
